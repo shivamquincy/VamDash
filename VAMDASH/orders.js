c744e2cdd40bc34343f1b8fc0413adbe48e7ef70
shivamquincy <95183060+shivamquincy@users.noreply.github.com>
2023-10-21 08:29:53 +0530

    added

---
+++ VAMDASH/orders.js
@@ -0,0 +1,31 @@
+const orders = [{
+    productName: " Drone with Camera Drone ",
+    productNumber: " 96996",
+    paymentStatus: "Paid ",
+    shipping: "Delivered "
+},
+{
+    productName: " GPS 4K Drone",
+    productNumber: "228221",
+    paymentStatus: "Paid ",
+    shipping: "Delivered "
+},
+{
+    productName: "Foldable Mini Drone",
+    productNumber: " 85631",
+    paymentStatus: "Due",
+    shipping: "Pending"
+},
+{
+    productName: " Lavender KF102 Drone ",
+    productNumber: " 36178",
+    paymentStatus: "Refunded",
+    shipping: "Declined"
+},
+{
+    productName: " Rukp F11 Drone ",
+    productNumber: " 49347",
+    paymentStatus: "Due ",
+    shipping: "Pending"
+},
+]
